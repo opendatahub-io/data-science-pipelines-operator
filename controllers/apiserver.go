8e3ba31d5f17bd09f95eb9dce09a6b1b0ef815d8
Humair Khan <humair88@hotmail.com>
2023-02-21 12:25:33 -0500

    Add handling of custom/provisioned secrets for db and storage.
    Signed-off-by: Humair Khan <humair88@hotmail.com>

--- controllers/apiserver.go
+++ controllers/apiserver.go
@@ -38,6 +38,7 @@ var apiServerTemplates = []string{
 	"apiserver/sa_pipeline-runner.yaml.tmpl",
 	"apiserver/service.yaml.tmpl",
 	"apiserver/deployment.yaml.tmpl",
+	"apiserver/mlpipeline-minio-artifact.yaml.tmpl",
 }
 
 const (
@@ -53,8 +54,7 @@ const (
 	defaultMinioScheme                 = "http"
 	DefaultObjectStoreConnectionSecure = false
 	// This is hardcoded in kfp-tekton, apiserver will always use this hardcoded secret for tekton resources
-	minioArtifactSecret     = "apiserver/mlpipeline-minio-artifact.yaml.tmpl"
-	minioArtifactSecretName = "mlpipeline-minio-artifact"
+	defaultObjectStorageSecretName = "mlpipeline-minio-artifact"
 )
 
 func passwordGen(n int) string {
@@ -67,8 +67,10 @@ func passwordGen(n int) string {
 	return string(b)
 }
 
-//SetupDBConnection sets up an DB Connection
-func (r *DSPipelineReconciler) SetupDBConnection(ctx context.Context, dsp *dspipelinesiov1alpha1.DSPipeline, params *DSPipelineParams) error {
+// SetupDBParams Populates the DB connection Parameters.
+// If an external secret is specified, SetupDBParams will retrieve DB credentials from it.
+// If DSPO is managing a dynamically created secret, then SetupDBParams generates the creds.
+func (r *DSPipelineReconciler) SetupDBParams(ctx context.Context, dsp *dspipelinesiov1alpha1.DSPipeline, params *DSPipelineParams) error {
 	//Set up DB Connection
 	usingExternalDB, err := params.UsingExternalDB(dsp)
 	if err != nil {
@@ -82,10 +84,10 @@ func (r *DSPipelineReconciler) SetupDBConnection(ctx context.Context, dsp *dspip
 		params.DBConnection.Username = dsp.Spec.Database.ExternalDB.Username
 		params.DBConnection.DBName = dsp.Spec.Database.ExternalDB.DBName
 		params.DBConnection.CredentialsSecret = dsp.Spec.Database.ExternalDB.PasswordSecret
-
 	} else {
 		params.DBConnection.Host = fmt.Sprintf(
-			"%s.%s.svc.cluster.local", defaultDBHostPrefix+"-"+params.Name,
+			"%s.%s.svc.cluster.local",
+			defaultDBHostPrefix+"-"+params.Name,
 			params.Namespace,
 		)
 		params.DBConnection.Port = defaultDBHostPort
@@ -113,34 +115,47 @@ func (r *DSPipelineReconciler) SetupDBConnection(ctx context.Context, dsp *dspip
 			Name: defaultDBHostPrefix + params.Name,
 			Key:  defaultDBSecretKey,
 		}
-		params.DBConnection.Password = passwordGen(12)
-	} else {
-		// Attempt to fetch the specified DB secret
-		dbSecret := &v1.Secret{}
-		namespacedName := types.NamespacedName{
-			Name:      params.DBConnection.CredentialsSecret.Name,
-			Namespace: params.Namespace,
-		}
-		err = r.Get(ctx, namespacedName, dbSecret)
-		if err != nil && apierrs.IsNotFound(err) {
-			r.Log.Error(err, fmt.Sprintf("Specified DB secret %s does not exist.", params.DBConnection.CredentialsSecret.Name))
+	}
+
+	dbSecret := &v1.Secret{}
+	namespacedName := types.NamespacedName{
+		Name:      params.DBConnection.CredentialsSecret.Name,
+		Namespace: params.Namespace,
+	}
+
+	createNewSecret := false
+
+	// Attempt to fetch the specified DB secret
+	err = r.Get(ctx, namespacedName, dbSecret)
+	if err != nil && apierrs.IsNotFound(err) {
+		if DBCredentialsNotSpecified {
+			params.DBConnection.Password = passwordGen(12)
+			dbSecret.StringData[defaultDBSecretKey] = params.DBConnection.Password
+			createNewSecret = true
+		} else {
+			r.Log.Error(err, fmt.Sprintf("DB secret %s was specified in CR but does not exist.",
+				params.DBConnection.CredentialsSecret.Name))
 			return err
-		} else if err != nil {
-			r.Log.Error(err, "Unable to fetch DB secret...")
-			return err
 		}
+	} else if err != nil {
+		r.Log.Error(err, "Unable to fetch DB secret...")
+		return err
+	}
+
+	if !createNewSecret {
 		params.DBConnection.Password, err = GetSecretDataDecoded(dbSecret, params.DBConnection.CredentialsSecret.Key)
 		if err != nil {
-			r.Log.Error(err, fmt.Sprintf("Encountered error when trying to fetch key %s in secret %s",
-				params.DBConnection.CredentialsSecret.Key, params.DBConnection.CredentialsSecret.Name))
 			return err
 		}
 	}
+
 	return nil
 }
 
-//SetupObjectStorageConnection sets up an Object Storage Connection
-func (r *DSPipelineReconciler) SetupObjectStorageConnection(ctx context.Context, dsp *dspipelinesiov1alpha1.DSPipeline, params *DSPipelineParams) error {
+// SetupObjectParams Populates the Object Storage connection Parameters.
+// If an external secret is specified, SetupObjectParams will retrieve storage credentials from it.
+// If DSPO is managing a dynamically created secret, then SetupObjectParams generates the creds.
+func (r *DSPipelineReconciler) SetupObjectParams(ctx context.Context, dsp *dspipelinesiov1alpha1.DSPipeline, params *DSPipelineParams) error {
 
 	usingExternalObjectStorage, err := params.UsingExternalStorage(dsp)
 	if err != nil {
@@ -157,7 +172,6 @@ func (r *DSPipelineReconciler) SetupObjectStorageConnection(ctx context.Context,
 		params.ObjectStorageConnection.Scheme = dsp.Spec.ObjectStorage.ExternalStorage.Scheme
 	} else {
 		params.ObjectStorageConnection.Bucket = dsp.Spec.ObjectStorage.Minio.Bucket
-
 		params.ObjectStorageConnection.Host = fmt.Sprintf(
 			"%s.%s.svc.cluster.local", defaultMinioHostPrefix+"-"+params.Name,
 			params.Namespace,
@@ -177,54 +191,71 @@ func (r *DSPipelineReconciler) SetupObjectStorageConnection(ctx context.Context,
 		params.ObjectStorageConnection.Port,
 	)
 
-	// TODO: there is an issue with how we're handling ObjectStorageConnection.CredentialsSecret
-	// We use this info to create the secret, but also fetch the custom secret, we cannot do both with the same info
-	// Use "CredentialsSecret" to create, and create a local variable populated with custom DSP values
-	// example customSecretName = dsp.spec.objectstorage.externalobjectstorage.secretkey etc...
-	DBCredentialsNotSpecified := reflect.DeepEqual(params.ObjectStorageConnection.CredentialsSecret, dspipelinesiov1alpha1.S3CredentialSecret{})
-	if DBCredentialsNotSpecified {
+	storageCredentialsNotSpecified := reflect.DeepEqual(params.ObjectStorageConnection.CredentialsSecret, dspipelinesiov1alpha1.S3CredentialSecret{})
+	if storageCredentialsNotSpecified {
 		// We assume validation ensures Storage Credentials are specified for External Object Storage
-		// So this case is only possible if Minio deployment is specified, but no secret is provided.
+		// So this case is only possible if Custom Object Storage deployment is enabled, but no secret is provided.
 		// In this case a custom secret will be created.
 		params.ObjectStorageConnection.CredentialsSecret = dspipelinesiov1alpha1.S3CredentialSecret{
-			SecretName: minioArtifactSecret,
+			SecretName: defaultObjectStorageSecretName,
 			AccessKey:  defaultObjectStorageAccessKey,
 			SecretKey:  defaultObjectStorageSecretKey,
 		}
-	} else {
-		// Attempt to fetch the specified DB secret
-		dbSecret := &v1.Secret{}
-		namespacedName := types.NamespacedName{
-			Name:      params.ObjectStorageConnection.CredentialsSecret.Name,
-			Namespace: params.Namespace,
-		}
-		err = r.Get(ctx, namespacedName, dbSecret)
-		if err != nil && apierrs.IsNotFound(err) {
-			r.Log.Error(err, fmt.Sprintf("Specified DB secret %s does not exist.", params.ObjectStorageConnection.CredentialsSecret.Name))
+	}
+
+	storageSecret := &v1.Secret{}
+	namespacedName := types.NamespacedName{
+		Name:      params.ObjectStorageConnection.CredentialsSecret.SecretName,
+		Namespace: params.Namespace,
+	}
+
+	createNewSecret := false
+
+	// Attempt to fetch the specified storage secret
+	err = r.Get(ctx, namespacedName, storageSecret)
+	if err != nil && apierrs.IsNotFound(err) {
+		if storageCredentialsNotSpecified {
+			params.ObjectStorageConnection.AccessKeyID = passwordGen(16)
+			params.ObjectStorageConnection.SecretAccessKey = passwordGen(24)
+			storageSecret.StringData[defaultObjectStorageAccessKey] = params.ObjectStorageConnection.AccessKeyID
+			storageSecret.StringData[defaultObjectStorageSecretKey] = params.ObjectStorageConnection.SecretAccessKey
+			err = r.Create(ctx, storageSecret)
+			createNewSecret = true
+		} else {
+			r.Log.Error(err, fmt.Sprintf("Storage secret %s was specified in CR but does not exist.",
+				params.ObjectStorageConnection.CredentialsSecret.SecretName))
 			return err
-		} else if err != nil {
-			r.Log.Error(err, "Unable to fetch DB secret...")
-			return err
 		}
-		params.ObjectStorageConnection.Password, err = GetSecretDataDecoded(dbSecret, params.ObjectStorageConnection.CredentialsSecret.Key)
+	} else if err != nil {
+		r.Log.Error(err, "Unable to fetch Storage secret...")
+		return err
+	}
+
+	if !createNewSecret {
+		params.ObjectStorageConnection.AccessKeyID, err = GetSecretDataDecoded(
+			storageSecret, params.ObjectStorageConnection.CredentialsSecret.AccessKey)
 		if err != nil {
-			r.Log.Error(err, fmt.Sprintf("Encountered error when trying to fetch key %s in secret %s",
-				params.ObjectStorageConnection.CredentialsSecret.Key, params.ObjectStorageConnection.CredentialsSecret.Name))
+			return err
+		}
+		params.ObjectStorageConnection.SecretAccessKey, err = GetSecretDataDecoded(
+			storageSecret, params.ObjectStorageConnection.CredentialsSecret.SecretKey)
+		if err != nil {
 			return err
 		}
 	}
+
 	return nil
 }
 
 func (r *DSPipelineReconciler) ReconcileAPIServer(ctx context.Context, dsp *dspipelinesiov1alpha1.DSPipeline, req ctrl.Request, params *DSPipelineParams) error {
 	r.Log.Info("Applying APIServer Resources")
 
-	err := r.SetupDBConnection(ctx, dsp, params)
+	err := r.SetupDBParams(ctx, dsp, params)
 	if err != nil {
 		return err
 	}
 
-	err = r.SetupObjectStorageConnection(ctx, dsp, params)
+	err = r.SetupObjectParams(ctx, dsp, params)
 	if err != nil {
 		return err
 	}
@@ -234,26 +265,6 @@ func (r *DSPipelineReconciler) ReconcileAPIServer(ctx context.Context, dsp *dspi
 		if err != nil {
 			return err
 		}
-	}
-
-	// Todo: We should switch to only using this secret for now otherwise the 2 storage secrets will go
-	// out of sync if one is deleted because the password will be re-generated.
-	// can also just check if the specified secret exists and copy those values onto the hardcoded secret.
-	secret := &v1.Secret{}
-	namespacedName := types.NamespacedName{
-		Name:      minioArtifactSecretName,
-		Namespace: req.Namespace,
-	}
-	err = r.Get(ctx, namespacedName, secret)
-	if err != nil && apierrs.IsNotFound(err) {
-		r.Log.Info("Specified minio artifact secret not found, creating...")
-		err := r.Apply(dsp, params, minioArtifactSecret)
-		if err != nil {
-			return err
-		}
-	} else if err != nil {
-		r.Log.Error(err, "Unable to fetch minio artifact secret...")
-		return err
 	}
 
 	r.Log.Info("Finished applying APIServer Resources")
