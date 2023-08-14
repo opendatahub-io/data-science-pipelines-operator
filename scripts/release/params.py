c38a65fbaa28164ff2195861f5325563baebc43c
Humair Khan <HumairAK@users.noreply.github.com>
2023-08-14 14:34:08 -0400

    Add release params generation utility.
    Signed-off-by: Humair Khan <HumairAK@users.noreply.github.com>

---
+++ scripts/release/params.py
@@ -0,0 +1,95 @@
+import sys
+
+import requests
+
+QUAY_REPOS = {
+    "IMAGES_APISERVER":  "ds-pipelines-api-server",
+    "IMAGES_ARTIFACT":  "ds-pipelines-artifact-manager",
+    "IMAGES_PERSISTENTAGENT":  "ds-pipelines-persistenceagent",
+    "IMAGES_SCHEDULEDWORKFLOW":  "ds-pipelines-scheduledworkflow",
+    "IMAGES_MLMDENVOY":  "ds-pipelines-metadata-envoy",
+    "IMAGES_MLMDGRPC":  "ds-pipelines-metadata-grpc",
+    "IMAGES_MLMDWRITER":  "ds-pipelines-metadata-writer",
+    "IMAGES_DSPO":  "data-science-pipelines-operator",
+
+}
+
+ARCH = "amd64"
+
+
+def fetch_quay_repo_tag_digest(quay_repo, quay_org, tag):
+    api_url = "https://quay.io/api/v1/repository/{0}/{1}/tag/?specificTag={2}".format(
+        quay_org,
+        quay_repo,
+        tag
+    )
+    response = requests.get(api_url).json()
+    tags = response['tags']
+
+    if len(tags) == 0 or 'end_ts' in tags[0]:
+        print("Tag does not exist or was deleted.", file=sys.stderr)
+        exit(1)
+    return tags[0]['manifest_digest']
+
+
+def fetch_rh_repo_tag_digest(repo, tag):
+    registry = "registry.access.redhat.com"
+    api_url = "https://catalog.redhat.com/api/containers/v1/repositories/registry/{0}/repository/{1}/tag/{2}".format(
+        registry,
+        repo,
+        tag
+    )
+    response = requests.get(api_url).json()
+
+    amd_img = {}
+    for img in response['data']:
+        if img['architecture'] == 'amd64':
+            amd_img = img
+
+    if not amd_img:
+        print("AMD64 arch image not found for repo {0} and tag {1}".format(repo, tag), file=sys.stderr)
+        exit(1)
+
+    sha_digest = amd_img['image_id']
+
+    return sha_digest
+
+
+def params(args):
+    tag = args.tag
+    quay_org = args.quay_org
+    file_out = args.out_file
+    ubi_minimal_tag = args.ubi_minimal_tag
+    ubi_micro_tag = args.ubi_micro_tag
+    mariadb_tag = args.mariadb_tag
+    oauth_proxy_tag = args.oauth_proxy_tag
+
+    images = []
+
+    # Fetch QUAY Images
+    for image_env_var in QUAY_REPOS:
+        image_repo = QUAY_REPOS[image_env_var]
+        digest = fetch_quay_repo_tag_digest(image_repo, quay_org, tag)
+        image_repo_with_digest = "{0}:{1}".format(image_repo, digest)
+        images.append("{0}=quay.io/opendatahub/{1}".format(image_env_var, image_repo_with_digest))
+
+    # Fetch RH Registry images
+    repo = "ubi8/ubi-minimal"
+    digest = fetch_rh_repo_tag_digest(repo, ubi_minimal_tag)
+    images.append("{0}=registry.access.redhat.com/{1}/{2}".format("IMAGES_CACHE", repo, digest))
+
+    repo = "ubi8/ubi-micro"
+    digest = fetch_rh_repo_tag_digest(repo, ubi_micro_tag)
+    images.append("{0}=registry.access.redhat.com/{1}/{2}".format("IMAGES_MOVERESULTSIMAGE", repo, digest))
+
+    repo = "rhel8/mariadb-103"
+    digest = fetch_rh_repo_tag_digest(repo, mariadb_tag)
+    images.append("{0}=registry.redhat.io/{1}/{2}".format("IMAGES_MARIADB", repo, digest))
+
+    repo = "openshift4/ose-oauth-proxy"
+    digest = fetch_rh_repo_tag_digest(repo, oauth_proxy_tag)
+    images.append("{0}=registry.redhat.io/{1}/{2}".format("IMAGES_OAUTHPROXY", repo, digest))
+
+    with open(file_out, 'w') as f:
+        for images in images:
+            f.write("{0}\n".format(images))
---
+++ scripts/release/release.py
@@ -0,0 +1,55 @@
+import argparse
+import os
+
+from scripts.release.params import params
+
+
+def env_opts(env: str):
+    if env in os.environ:
+        return {'default': os.environ[env]}
+    else:
+        return {'required': True}
+
+
+def version_doc(args):
+    input_file = args.input_file
+    output_file = args.out_file
+    print("generating compatibility md {0}".format(output_file))
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="DSP Release Tools."
+    )
+
+    subparsers = parser.add_subparsers(help='sub-command help', required=True)
+
+    # Params.env generator inputs
+    parser_params = subparsers.add_parser('params', help='Params.env generator inputs')
+    parser_params.set_defaults(func=params)
+    parser_params.add_argument('--tag', type=str, required=True, help='Tag for which to fed image digests for.')
+    parser_params.add_argument('--quay_org', default="opendatahub-io", type=str,
+                               help='Tag for which to fed image digests for.')
+    parser_params.add_argument('--out_file', default='params.env', type=str, help='File path output for params.env')
+    parser.add_argument("--ubi-minimal", dest="ubi_minimal_tag", default="8.8",
+                        help="ubi-minimal version tag in rh registry")
+    parser.add_argument("--ubi-micro", dest="ubi_micro_tag", default="8.8",
+                        help="ubi-micro version tag in rh registry")
+    parser.add_argument("--mariadb", dest="mariadb_tag", default="1",
+                        help="mariadb version tag in rh registry")
+    parser.add_argument("--oauthproxy", dest="oauth_proxy_tag", default="v4.10",
+                        help="oauthproxy version tag in rh registry")
+
+    # Version Compatibility Matrix doc generator
+    parser_vd = subparsers.add_parser('version_doc', help='Version Compatibility Matrix doc generator')
+    parser_vd.set_defaults(func=version_doc)
+    parser_vd.add_argument('--out_file', default='compatibility.md', type=str, help='File output for markdown doc.')
+    parser_vd.add_argument('--input_file', default='compatibility.yaml', type=str,
+                           help='Yaml input for compatibility doc generation.')
+
+    args = parser.parse_args()
+    args.func(args)
+
+
+if __name__ == "__main__":
+    main()
