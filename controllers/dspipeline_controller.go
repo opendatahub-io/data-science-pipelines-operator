043f3eb1f808b0dd6f1557992de208f43334b411
Giulio Frasca <gfrasca@redhat.com>
2023-09-14 15:44:03 -0400

    Add ApplyDir Reconciler function, Dynamically retrieve templates

--- controllers/dspipeline_controller.go
+++ controllers/dspipeline_controller.go
@@ -64,6 +64,14 @@ func (r *DSPAReconciler) ApplyDir(owner mf.Owner, params *DSPAParams, directory 
 	return r.ApplyAll(owner, params, templates)
 }
 
+func (r *DSPAReconciler) ApplyDir(owner mf.Owner, params *DSPAParams, directory string, fns ...mf.Transformer) error {
+	templates, err := util.GetTemplatesInDir(r.TemplatesPath, directory)
+	if err != nil {
+		return err
+	}
+	return r.ApplyAll(owner, params, templates)
+}
+
 func (r *DSPAReconciler) ApplyAll(owner mf.Owner, params *DSPAParams, templates []string, fns ...mf.Transformer) error {
 	for _, template := range templates {
 		err := r.Apply(owner, params, template)
