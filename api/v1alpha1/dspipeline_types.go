94adfa6005eac41d6f12c3b10113df0358452f4e
Giulio Frasca <gfrasca@redhat.com>
2023-09-27 18:28:14 -0400

    Implement DSPVersion item in DSPA CRD

--- api/v1alpha1/dspipeline_types.go
+++ api/v1alpha1/dspipeline_types.go
@@ -44,6 +44,9 @@ type DSPASpec struct {
 	// +kubebuilder:validation:Optional
 	// +kubebuilder:default:={deploy: false}
 	*VisualizationServer `json:"visualizationServer"`
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default:="v1"
+	DSPVersion string `json:"dspVersion,omitempty"`
 }
 
 type APIServer struct {
