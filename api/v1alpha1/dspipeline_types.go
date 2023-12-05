8749630b5e6379f0748dd4e3f2d7df8aa2214007
Giulio Frasca <gfrasca@redhat.com>
2023-12-05 14:42:14 -0500

    Add Context Documentation for WorkflowController
    - Add description and code comments regarding
      Workflow Controller and its image to the CRD and DSPO config

--- api/v1alpha1/dspipeline_types.go
+++ api/v1alpha1/dspipeline_types.go
@@ -51,6 +51,7 @@ type DSPASpec struct {
 	// +kubebuilder:validation:Optional
 	// +kubebuilder:default:="v1"
 	DSPVersion string `json:"dspVersion,omitempty"`
+	// WorkflowController is an argo-specific component that manages a DSPA's Workflow objects and handles the orchestration of them with the central Argo server
 	// +kubebuilder:validation:Optional
 	// +kubebuilder:default:={deploy: false}
 	*WorkflowController `json:"workflowController,omitempty"`
