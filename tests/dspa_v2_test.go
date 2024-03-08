c9ccb422d0a673c6e72319ab1215c36528afadc0
vmudadla <vmudadla@redhat.com>
2024-03-08 15:40:14 -0600

    Add integration tests for external storage/db connections
    Update Kind file
    
    Update kind file
    
    Update Kind file
    
    Update Kind file
    
    Update Kind file
    
    Update Kind file
    
    Update Kind file
    
    Update kind
    
    Update Kind workflow
    
    Fixed pre-commit check issues
    
    Update dspa
    
    Update PVC
    
    Update maridb image
    
    Update resource files
    
    Update Kind file
    
    Update Kind file
    
    Update Kind file
    
    Update kind file
    
    Update kind file
    
    Update dspa
    
    Fix pre-commit issues
    
    Update kind file
    
    Update file
    
    Update Kind file
    
    Update kind file
    
    Update dspa
    
    Update dspa
    
    Update dspa
    
    Update dspa
    
    Update Kind-integration
    
    Update Kind file
    
    Update dspa
    
    Fix pre-commit issues
    
    Fix white spaces
    
    Fix white spaces
    
    Updated Githubaction workflow
    
    Updated Githubaction workflow

--- tests/dspa_v2_test.go
+++ tests/dspa_v2_test.go
@@ -20,6 +20,7 @@ package integration
 
 import (
 	"fmt"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	systemsTesttUtil "github.com/opendatahub-io/data-science-pipelines-operator/tests/util"
@@ -29,7 +30,16 @@ import (
 
 var _ = Describe("A successfully deployed DSPA", func() {
 
-	podCount := 8
+	podCount := 6
+
+	var isTestDspa bool
+
+	BeforeEach(func() {
+		isTestDspa = DSPA.Name == "test-dspa"
+		if isTestDspa {
+			podCount = 8
+		}
+	})
 
 	Context("with default MariaDB and Minio", func() {
 		It(fmt.Sprintf("should have %d pods", podCount), func() {
@@ -51,11 +61,16 @@ var _ = Describe("A successfully deployed DSPA", func() {
 		It(fmt.Sprintf("should have a ready %s deployment", "Scheduled Workflow"), func() {
 			systemsTesttUtil.TestForSuccessfulDeployment(ctx, DSPANamespace, fmt.Sprintf("ds-pipeline-scheduledworkflow-%s", DSPA.Name), clientmgr.k8sClient)
 		})
-		It(fmt.Sprintf("should have a ready %s deployment", "MariaDB"), func() {
-			systemsTesttUtil.TestForSuccessfulDeployment(ctx, DSPANamespace, fmt.Sprintf("mariadb-%s", DSPA.Name), clientmgr.k8sClient)
-		})
-		It(fmt.Sprintf("should have a ready %s deployment", "Minio"), func() {
-			systemsTesttUtil.TestForSuccessfulDeployment(ctx, DSPANamespace, fmt.Sprintf("minio-%s", DSPA.Name), clientmgr.k8sClient)
-		})
+		if isTestDspa {
+			It(fmt.Sprintf("should have a ready %s deployment", "MariaDB"), func() {
+				systemsTesttUtil.TestForSuccessfulDeployment(ctx, DSPANamespace, fmt.Sprintf("mariadb-%s", DSPA.Name), clientmgr.k8sClient)
+			})
+		}
+		if isTestDspa {
+			It(fmt.Sprintf("should have a ready %s deployment", "Minio"), func() {
+				systemsTesttUtil.TestForSuccessfulDeployment(ctx, DSPANamespace, fmt.Sprintf("minio-%s", DSPA.Name), clientmgr.k8sClient)
+			})
+		}
 	})
+
 })
