5fe2b21a474aa19766f9ae067133e413ac263cab
hbelmiro <helber.belmiro@gmail.com>
2024-06-12 11:42:35 -0300

    Fixed flaky test IntegrationTestSuite#TestPipelineSuccessfulRun
    Signed-off-by: hbelmiro <helber.belmiro@gmail.com>

--- tests/pipeline_runs_test.go
+++ tests/pipeline_runs_test.go
@@ -26,7 +26,6 @@ import (
 	"testing"
 
 	TestUtil "github.com/opendatahub-io/data-science-pipelines-operator/tests/util"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -35,7 +34,8 @@ func (suite *IntegrationTestSuite) TestPipelineSuccessfulRun() {
 	suite.T().Run("Should create a Pipeline Run", func(t *testing.T) {
 		// Retrieve Pipeline ID to create a new run
 		pipelineDisplayName := "[Demo] iris-training"
-		pipelineID := TestUtil.RetrievePipelineId(t, APIServerURL, pipelineDisplayName)
+		pipelineID, err := TestUtil.RetrievePipelineId(t, APIServerURL, pipelineDisplayName)
+		require.NoError(t, err)
 		postUrl := fmt.Sprintf("%s/apis/v2beta1/runs", APIServerURL)
 		body := TestUtil.FormatRequestBody(t, pipelineID, pipelineDisplayName)
 		contentType := "application/json"
@@ -46,7 +46,7 @@ func (suite *IntegrationTestSuite) TestPipelineSuccessfulRun() {
 		responseString := string(responseData)
 		loggr.Info(responseString)
 		require.NoError(t, err)
-		assert.Equal(t, 200, response.StatusCode)
+		require.Equal(t, 200, response.StatusCode)
 
 		err = TestUtil.WaitForPipelineRunCompletion(t, APIServerURL)
 		require.NoError(t, err)
@@ -55,7 +55,8 @@ func (suite *IntegrationTestSuite) TestPipelineSuccessfulRun() {
 	suite.T().Run("Should create a Pipeline Run using custom pip server", func(t *testing.T) {
 		// Retrieve Pipeline ID to create a new run
 		pipelineDisplayName := "Test pipeline run with custom pip server"
-		pipelineID := TestUtil.RetrievePipelineId(t, APIServerURL, pipelineDisplayName)
+		pipelineID, err := TestUtil.RetrievePipelineId(t, APIServerURL, pipelineDisplayName)
+		require.NoError(t, err)
 		postUrl := fmt.Sprintf("%s/apis/v2beta1/runs", APIServerURL)
 		body := TestUtil.FormatRequestBody(t, pipelineID, pipelineDisplayName)
 		contentType := "application/json"
@@ -66,7 +67,7 @@ func (suite *IntegrationTestSuite) TestPipelineSuccessfulRun() {
 		responseString := string(responseData)
 		loggr.Info(responseString)
 		require.NoError(t, err)
-		assert.Equal(t, 200, response.StatusCode)
+		require.Equal(t, 200, response.StatusCode)
 
 		err = TestUtil.WaitForPipelineRunCompletion(t, APIServerURL)
 		require.NoError(t, err)
