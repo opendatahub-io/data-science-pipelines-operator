12fce7781b5e68cbeac1c3ba6cc202a4a9b9861c
mprahl <mprahl@users.noreply.github.com>
2025-03-06 16:16:40 -0500

    Pin the KFP version in the custom PyPi integration test
    The pipelines use KFP 2.11.0 so we need that version to be present on
    the PyPi server.
    
    Signed-off-by: mprahl <mprahl@users.noreply.github.com>

--- .github/scripts/python_package_upload/package_download.sh
+++ .github/scripts/python_package_upload/package_download.sh
@@ -4,7 +4,7 @@ set -ex
 
 # Download packages
 for python_version in "3.9" "3.10" "3.11" "3.12"; do
-  for package in "kfp" "numpy"; do
+  for package in "kfp==2.11.0" "numpy"; do
     # If we don't set the --python it will use the one from the computer that may not be the one that the
     # pipeline is running
     pip download $package -d packages --only-binary=:none: --python $python_version
