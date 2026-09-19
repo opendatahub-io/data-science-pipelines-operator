9801cd2ebbec3a8be87b0564c2954404ca9fdcd9
lugi0 <lgiorgi@redhat.com>
2026-09-19 19:57:14 +0200

    test(argo): parse ownership markers structurally

--- config/argo/assets_test.go
+++ config/argo/assets_test.go
@@ -22,6 +22,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"sigs.k8s.io/yaml"
 )
 
 func TestObjects(t *testing.T) {
@@ -65,8 +66,21 @@ func TestArgoCRDBootstrapIncludesOwnershipMarkers(t *testing.T) {
 	data, err := os.ReadFile("crds/kustomization.yaml")
 	require.NoError(t, err)
 
-	content := string(data)
-	require.Contains(t, content, `app.opendatahub.io/data-science-pipelines-operator: "true"`)
-	require.Contains(t, content, "dsp-version: v2")
-	require.Contains(t, content, `aipipelines.components.platform.opendatahub.io/argo-managed: "true"`)
+	var kustomization struct {
+		Labels []struct {
+			Pairs map[string]string `json:"pairs"`
+		} `json:"labels"`
+		CommonAnnotations map[string]string `json:"commonAnnotations"`
+	}
+	require.NoError(t, yaml.Unmarshal(data, &kustomization))
+
+	labelPairs := make(map[string]string)
+	for _, label := range kustomization.Labels {
+		for key, value := range label.Pairs {
+			labelPairs[key] = value
+		}
+	}
+	require.Equal(t, "true", labelPairs["app.opendatahub.io/data-science-pipelines-operator"])
+	require.Equal(t, "v2", labelPairs["dsp-version"])
+	require.Equal(t, "true", kustomization.CommonAnnotations["aipipelines.components.platform.opendatahub.io/argo-managed"])
 }
